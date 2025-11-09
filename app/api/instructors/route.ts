e58cd79e74d33d701539be1972dcfdb0b13a8d80
Kushal Baranwal <imkushal01@gmail.com>
2025-11-08 17:33:39 -0800

    fixed formatting issues

--- app/api/instructors/route.ts
+++ app/api/instructors/route.ts
@@ -3,49 +3,51 @@ import { BaseCourseRow, CourseRow } from "@/app/types";
 import { NextResponse } from "next/server";
 
 type InstructorIndex = {
-    [instructorName: string]: CourseRow[];
+  [instructorName: string]: CourseRow[];
 };
 
 const instructorIndex = untypedInstructorIndex as InstructorIndex;
 
 export type CoursesByInstructor = {
-    [instructor: string]: {
-        [subjectArea: string]: {
-            [catalogNumber: string]: BaseCourseRow & {
-                nRows: number;
-            };
-        };
+  [instructor: string]: {
+    [subjectArea: string]: {
+      [catalogNumber: string]: BaseCourseRow & {
+        nRows: number;
+      };
     };
+  };
 };
 
 export async function GET() {
-    const coursesByInstructor: CoursesByInstructor = {};
-    const instructors = Object.keys(instructorIndex);
-    for (const instructor of instructors) {
-        if (!coursesByInstructor[instructor]) {
-            coursesByInstructor[instructor] = {};
-        }
-
-        for (const course of instructorIndex[instructor]) {
-            const { subjectArea } = course;
-            if (!coursesByInstructor[instructor][subjectArea]) {
-                coursesByInstructor[instructor][subjectArea] = {};
-            }
-            if (!coursesByInstructor[instructor][subjectArea][course.catalogNumber]) {
-                coursesByInstructor[instructor][subjectArea][course.catalogNumber] = {
-                    subjectArea,
-                    courseTitle: course.courseTitle,
-                    catalogNumber: course.catalogNumber,
-                    nRows: 1,
-                };
-            } else {
-                coursesByInstructor[instructor][subjectArea][course.catalogNumber].nRows += 1;
-            }
-            coursesByInstructor[instructor][subjectArea];
-        }
+  const coursesByInstructor: CoursesByInstructor = {};
+  const instructors = Object.keys(instructorIndex);
+  for (const instructor of instructors) {
+    if (!coursesByInstructor[instructor]) {
+      coursesByInstructor[instructor] = {};
     }
 
-    return NextResponse.json(coursesByInstructor);
+    for (const course of instructorIndex[instructor]) {
+      const { subjectArea } = course;
+      if (!coursesByInstructor[instructor][subjectArea]) {
+        coursesByInstructor[instructor][subjectArea] = {};
+      }
+      if (!coursesByInstructor[instructor][subjectArea][course.catalogNumber]) {
+        coursesByInstructor[instructor][subjectArea][course.catalogNumber] = {
+          subjectArea,
+          courseTitle: course.courseTitle,
+          catalogNumber: course.catalogNumber,
+          nRows: 1,
+        };
+      } else {
+        coursesByInstructor[instructor][subjectArea][
+          course.catalogNumber
+        ].nRows += 1;
+      }
+      coursesByInstructor[instructor][subjectArea];
+    }
+  }
+
+  return NextResponse.json(coursesByInstructor);
 }
 
 export type Response = CoursesByInstructor;
--- app/components/search/SubjectAreaQueryResults.tsx
+++ app/components/search/SubjectAreaQueryResults.tsx
@@ -3,65 +3,71 @@ import { matchSubjectArea, getSubjectAreaLongName } from "@/app/utils";
 import { QueryResults } from "./QueryResults";
 
 type SubjectAreaQueryResultsProps = {
-    courses: Response;
-    /**
-     * The subject area query
-     */
-    query: string;
-    /**
-     * Called when the user selects a subject area
-     * @param subjectArea the subject area that the user selected
-     *  from the query results
-     */
-    onSelectSubjectArea(subjectArea: string): void;
+  courses: Response;
+  /**
+   * The subject area query
+   */
+  query: string;
+  /**
+   * Called when the user selects a subject area
+   * @param subjectArea the subject area that the user selected
+   *  from the query results
+   */
+  onSelectSubjectArea(subjectArea: string): void;
 };
 
-const SubjectAreaQueryResults = ({ courses, query, onSelectSubjectArea }: SubjectAreaQueryResultsProps) => {
-    const subjectAreas = Object.keys(courses);
+const SubjectAreaQueryResults = ({
+  courses,
+  query,
+  onSelectSubjectArea,
+}: SubjectAreaQueryResultsProps) => {
+  const subjectAreas = Object.keys(courses);
 
-    /**
-     * Given a query, returns a function which checks whether a
-     * `subjectArea` (short form) could match the `query`.
-     *
-     * TODO(nathanhleung):
-     * - take into account edit distance?
-     * - rank results somehow, e.g. "com sci" should be first result
-     *   for "cs", not "classics"
-     * - stream results, since the last check (nested array) could be expensive?
-     * - maybe even just sort by number of courses available
-     *
-     * @param query the search query
-     * @returns a matcher function which accepts a `subjectArea` and
-     *  returns whether the given query matches the subject area.
-     */
+  /**
+   * Given a query, returns a function which checks whether a
+   * `subjectArea` (short form) could match the `query`.
+   *
+   * TODO(nathanhleung):
+   * - take into account edit distance?
+   * - rank results somehow, e.g. "com sci" should be first result
+   *   for "cs", not "classics"
+   * - stream results, since the last check (nested array) could be expensive?
+   * - maybe even just sort by number of courses available
+   *
+   * @param query the search query
+   * @returns a matcher function which accepts a `subjectArea` and
+   *  returns whether the given query matches the subject area.
+   */
 
-    return (
-        <QueryResults
-            data={subjectAreas}
-            query={query}
-            matcher={(query) => matchSubjectArea(query, courses)}
-            onSelectResult={onSelectSubjectArea}
-            noResultsMessage="No departments found matching your query"
-            renderResult={(subjectArea) => {
-                const nCourses = Object.values(courses[subjectArea]).length;
+  return (
+    <QueryResults
+      data={subjectAreas}
+      query={query}
+      matcher={(query) => matchSubjectArea(query, courses)}
+      onSelectResult={onSelectSubjectArea}
+      noResultsMessage="No departments found matching your query"
+      renderResult={(subjectArea) => {
+        const nCourses = Object.values(courses[subjectArea]).length;
 
-                return (
-                    <div className="text-black bg-white cursor-pointer p-4 border-t-gray-200 border-t-2">
-                        <div className="flex">
-                            <div className="flex-1">
-                                <h3 className="text-2xl font-bold">{subjectArea}</h3>
-                                <p className="text-xs">{getSubjectAreaLongName(subjectArea)}</p>
-                            </div>
-                            <div className="text-center">
-                                <h3 className="text-2xl">{nCourses}</h3>
-                                <p className="text-xs">{nCourses === 1 ? "course" : "courses"}</p>
-                            </div>
-                        </div>
-                    </div>
-                );
-            }}
-        />
-    );
+        return (
+          <div className="text-black bg-white cursor-pointer p-4 border-t-gray-200 border-t-2">
+            <div className="flex">
+              <div className="flex-1">
+                <h3 className="text-2xl font-bold">{subjectArea}</h3>
+                <p className="text-xs">{getSubjectAreaLongName(subjectArea)}</p>
+              </div>
+              <div className="text-center">
+                <h3 className="text-2xl">{nCourses}</h3>
+                <p className="text-xs">
+                  {nCourses === 1 ? "course" : "courses"}
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      }}
+    />
+  );
 };
 
 export { SubjectAreaQueryResults };
